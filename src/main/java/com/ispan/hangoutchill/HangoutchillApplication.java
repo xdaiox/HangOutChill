87e2c7576a6d967342bf1ec8854f96cd15130608
daio <tttmmmmm@gmail.com>
2023-04-20 09:51:28 +0800

    create SpringBoot Project.

---
+++ src/main/java/com/ispan/hangoutchill/HangoutchillApplication.java
@@ -0,0 +1,13 @@
+package com.ispan.hangoutchill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangoutchillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangoutchillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ispan/hangoutchill/HangoutchillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ispan.hangoutchill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangoutchillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
