fb51158c145b0e41017232b6060fc9bb0de6219a
DESKTOP-RAY\RAY <william861213@gmail.com>
2023-04-22 17:29:11 +0800

    新增一些location相關model

---
+++ src/main/java/com/ispan/hangoutchill/location/model/LocationComment.java
@@ -0,0 +1,79 @@
+package com.ispan.hangoutchill.location.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table(name="locationComment")
+public class LocationComment {
+
+	@Id
+    @Column(name = "comment_id")
+    private Integer cmtID;
+	
+    @Column(name = "member_id")
+    private Integer mbrID;
+    @Column(name="location_id")
+    private Integer locID;
+    @Column(name = "comment_rating")
+    private String cmtRating;
+    @Column(name = "comment_title")
+    private String cmtTitle;
+    @Column(name = "comment_content")
+    private String cmtContent;
+    @Column(name="comment_time")
+    private String cmtUpdateTime;
+    
+    
+    
+	public Integer getCmtID() {
+		return cmtID;
+	}
+	public void setCmtID(Integer cmtID) {
+		this.cmtID = cmtID;
+	}
+	public Integer getMbrID() {
+		return mbrID;
+	}
+	public void setMbrID(Integer mbrID) {
+		this.mbrID = mbrID;
+	}
+	public Integer getLocID() {
+		return locID;
+	}
+	public void setLocID(Integer locID) {
+		this.locID = locID;
+	}
+	public String getCmtRating() {
+		return cmtRating;
+	}
+	public void setCmtRating(String cmtRating) {
+		this.cmtRating = cmtRating;
+	}
+	public String getCmtTitle() {
+		return cmtTitle;
+	}
+	public void setCmtTitle(String cmtTitle) {
+		this.cmtTitle = cmtTitle;
+	}
+	public String getCmtContent() {
+		return cmtContent;
+	}
+	public void setCmtContent(String cmtContent) {
+		this.cmtContent = cmtContent;
+	}
+	public String getCmtUpdateTime() {
+		return cmtUpdateTime;
+	}
+	public void setCmtUpdateTime(String cmtUpdateTime) {
+		this.cmtUpdateTime = cmtUpdateTime;
+	}
+	
+    
+    
+
+    
+}
